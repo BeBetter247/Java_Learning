180dbee90a322068ea75ed1e903834b1b17a9eb0
BeBetter247 <2417483920@qq.com>
2025-08-10 22:36:14 +0800

    更新字符类型输出

---
+++ literal/literal.java
@@ -0,0 +1,25 @@
+public class literal{
+    public static void main(String[] args){
+        System.out.println("start output literal:");
+        printliteral();
+
+    }
+    public static void printliteral(){
+        int a=10;
+        double b=10.0;
+        long c=10000000000L;
+        float d=10.0f;
+        char e='A';
+        boolean f=true;
+        String g="Hello, World!";
+        System.out.println("int: " + a);
+        System.out.println("double: " + b);
+        System.out.println("long: " + c);
+        System.out.println("float: " + d);
+        System.out.println("char: " + e);
+        System.out.println("boolean: " + f);
+        System.out.println("String: " + g);
+        System.out.println("end output literal:");
+
+    }
+}
